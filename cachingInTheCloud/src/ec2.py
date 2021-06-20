14297c722981600e6716d09f3dc1a6f6821880ce
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-20 20:02:46 +0300

    Update ec2.py

--- cachingInTheCloud/src/ec2.py
+++ cachingInTheCloud/src/ec2.py
@@ -1,13 +1,13 @@
 import boto3
 import sys
-from src.elb import create_ec2_instances, register_instance_in_elb
+import elb
 
 if __name__=="__main__":
-    instances = create_ec2_instances(sys.argv[1])
+    instances = elb.create_ec2_instances(sys.argv[1])
                                               
     for i in range(len(instances["Instances"])):
         instance_id = instances["Instances"][i]["InstanceId"]
         ec2_resource = boto3.resource('ec2')
         instance = ec2_resource.Instance(instance_id) 
         instance.wait_until_running()
-        register_instance_in_elb(instance_id)
+        elb.register_instance_in_elb(instance_id)
