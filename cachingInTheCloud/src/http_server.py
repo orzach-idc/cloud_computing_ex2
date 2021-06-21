5fcfda0328cb4cf5133fc5965888fc0fceee334d
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 18:18:01 +0300

    Update http_server.py

--- cachingInTheCloud/src/http_server.py
+++ cachingInTheCloud/src/http_server.py
@@ -8,12 +8,19 @@ instance_cache = dict()
 
 def get_live_nodes():
 #     TODO - implement
-    return []
+    pass
+
+def hash_func():
+#     TODO - implement
+    pass
 
 def redirect_request(request_type, ip):
 #     TODO - implement
     pass
 
+def create_http_request_template(request_type):
+    pass
+    
 def write_request_handler(str_key, data, expiration_date):
     instance_cache[str_key] = [data, expiration_date]
     return "succeeded"
@@ -43,7 +50,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         if f.path == "/read":
             response = read_request_handler(f.args['str_key'])
             self.wfile.write("get request = {}".format(response).encode('utf-8'))
-            
+        elif f.path == "/get":
+#             send read request to 2 ec2 by getting ip from hash func
         elif self.path == "/healthcheck":
             self.wfile.write("Ok".format().encode('utf-8'))
         
@@ -54,6 +62,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         if f.path =="/write":
             response = write_request_handler(f.args['str_key'], f.args['data'], f.args['expiration_date'])
             self.wfile.write("put request {}".format(response).encode('utf-8'))
+        elif f.path == "/get":
+#             send write request to 2 ec2 by getting ip from hash func
 
 HTTPServer((host, port), HandleRequests).serve_forever()
 
