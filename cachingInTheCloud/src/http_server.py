321d462401aa13f90fc3ede1b1f1d21ce14722a7
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 13:08:28 +0300

    Update http_server.py

--- cachingInTheCloud/src/http_server.py
+++ cachingInTheCloud/src/http_server.py
@@ -8,7 +8,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write("GET request for {'aaa'}".format(self.path).encode('utf-8'))
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
         
     def do_POST(self):
         '''Reads post request body'''
