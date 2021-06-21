488870cca1ac770dd425e9526686b534abfa7975
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 17:24:08 +0300

    Create 1ec2_init.sh

---
+++ cachingInTheCloud/src/1ec2_init.sh
@@ -0,0 +1,14 @@
+# update packages
+sudo apt update
+
+# installing pip for python 3
+sudo apt install python3-pip
+
+# installing aws cli
+sudo pip3 install --upgrade awscli
+
+# installing aws sam 
+sudo pip3 install aws-sam-cli
+
+#installing furl
+pip install furl 
