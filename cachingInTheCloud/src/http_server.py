192bdf6fd5141826ce1cad53e89d3801732bd2f0
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 18:08:46 +0300

    Update http_server.py

--- cachingInTheCloud/src/http_server.py
+++ cachingInTheCloud/src/http_server.py
@@ -5,7 +5,6 @@ from furl import furl
 host = ''
 port = 80
 instance_cache = dict()
-instance_cache['test'] = ('orhilla', "22-06-2021")
 
 def get_live_nodes():
 #     TODO - implement
@@ -17,7 +16,7 @@ def redirect_request(request_type, ip):
 
 def write_request_handler(str_key, data, expiration_date):
     instance_cache[str_key] = [data, expiration_date]
-    return
+    return "succeeded"
 
 def read_request_handler(str_key):
     tup = instance_cache.get(str_key, None)
@@ -40,20 +39,21 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         f = furl(self.path)
-        if f.path == "/get":
+        
+        if f.path == "/read":
             response = read_request_handler(f.args['str_key'])
             self.wfile.write("get request = {}".format(response).encode('utf-8'))
+            
         elif self.path == "/healthcheck":
             self.wfile.write("Ok".format().encode('utf-8'))
         
     def do_POST(self):
-        '''Reads post request body'''
         self._set_headers()
-        if self.path =="/put":
-#             content_len = int(self.headers.getheader('content-length', 0))
-#             post_body = self.rfile.read(content_len)
-#             self.wfile.write("received post request:<br>{}".format(post_body))
-            self.wfile.write("put request".format().encode('utf-8'))
+        f = furl(self.path)
+        
+        if f.path =="/write":
+            response = write_request_handler(f.args['str_key'], f.args['data'], f.args['expiration_date'])
+            self.wfile.write("put request {}".format(response).encode('utf-8'))
 
 HTTPServer((host, port), HandleRequests).serve_forever()
 
