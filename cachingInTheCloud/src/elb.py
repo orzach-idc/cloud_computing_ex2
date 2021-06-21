a3eb04682b7965052561b740ee0e0374a9ec2107
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 19:38:00 +0300

    Update elb.py

--- cachingInTheCloud/src/elb.py
+++ cachingInTheCloud/src/elb.py
@@ -6,13 +6,8 @@ PREFIX="cache-elb"
 
 elb = boto3.client('elbv2')
 ec2 = boto3.client('ec2')
-ec2_user_data = """#!/bin/bash
-    git clone https://github.com/orzach-idc/cloud_computing_ex2.git
-    cd cloud_computing_ex2/cachingInTheCloud/src
-    chmod 777 *.sh
-    ./ec2_init.sh 
-    sudo python3 test.py
-"""
+ec2_user_data = '''#!/bin/bash
+echo 'test' > /tmp/hello'''
 
 def init_security_groups(vpc_id):
     try:
