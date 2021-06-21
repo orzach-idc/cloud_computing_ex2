566d72ea40cfa10d7619fa63a5173b5abdaeb413
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 17:58:41 +0300

    Update http_server.py

--- cachingInTheCloud/src/http_server.py
+++ cachingInTheCloud/src/http_server.py
@@ -23,8 +23,8 @@ def read_request_handler(str_key):
     tup = instance_cache.get(str_key, None)
     cur_date = datetime.strptime(tup[1], '%d-%m-%Y')
     if tup != None:
-        return 1
         if cur_date <= datetime.now():
+            return 1
             return tup[0]
         else:
             instance_cache.pop(str_key)
