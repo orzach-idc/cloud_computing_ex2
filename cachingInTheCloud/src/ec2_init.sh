4f1c8a52de6fe5a15e3b61b7a1105f07135395ee
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 20:18:39 +0300

    Update ec2_init.sh

--- cachingInTheCloud/src/ec2_init.sh
+++ cachingInTheCloud/src/ec2_init.sh
@@ -5,10 +5,10 @@ sudo apt update
 sudo apt --yes --force-yes install python3-pip
 
 # installing aws cli
-sudo pip3 --yes --force-yes install --upgrade awscli
+sudo pip3 install --upgrade awscli
 
 # installing aws sam 
-sudo pip3  --yes --force-yes install aws-sam-cli
+sudo pip3 install aws-sam-cli
 
 #installing furl
-sudo pip --yes --force-yes install furl 
+sudo pip install furl 
