568751d5c046f9273c494c6c49b2122e0ddae89f
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-21 12:59:07 +0300

    Update http_server.py

--- cachingInTheCloud/src/http_server.py
+++ cachingInTheCloud/src/http_server.py
@@ -1,26 +1,26 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer # python2
+from http.server import BaseHTTPRequestHandler, HTTPServer # python3
 
-from io import BytesIO
-
-
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+class HandleRequests(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
+        self._set_headers()
+        self.wfile.write("received get request")
+        
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
+        '''Reads post request body'''
+        self._set_headers()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        self.wfile.write("received post request:<br>{}".format(post_body))
 
+    def do_PUT(self):
+        self.do_POST()
 
-httpd = HTTPServer(('localhost', 80), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+host = ''
+port = 80
+HTTPServer((host, port), HandleRequests).serve_forever()
