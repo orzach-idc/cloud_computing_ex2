27e73bdea87a7fe677579ba1f32fa7a5161116c6
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-22 12:50:20 +0300

    Update set_environ_variables.py

--- cachingInTheCloud/src/set_environ_variables.py
+++ cachingInTheCloud/src/set_environ_variables.py
@@ -13,3 +13,5 @@ aws_default_region = config_file.readline().split('= ')[1]
 os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key_id
 os.environ["AWS_SECRET_ACCESS_KEY_ID"] = aws_secret_access_key_id
 os.environ["AWS_DEFAULT_REGION"] = aws_default_region
+
+print(os.environ.get("AWS_ACCESS_KEY_ID""))
