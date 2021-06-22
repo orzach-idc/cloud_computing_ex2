c1909e8342656280be80cb6057135e61f33f150c
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-22 18:18:52 +0300

    Update ec2_server.py

--- cachingInTheCloud/src/ec2_server.py
+++ cachingInTheCloud/src/ec2_server.py
@@ -30,7 +30,7 @@ def get_request_handler(ip1, ip2, request_args):
 #     response2 = None
     request1 = f"http://{ip1}/read"
 #     request2 = f"http://{ip2}/read"
-    response1 = requests.get(request1, request_args)
+    response1 = requests.get(request1, params = request_args)
 #     response2 = request.get(request2, request_args)
     
     return response1
