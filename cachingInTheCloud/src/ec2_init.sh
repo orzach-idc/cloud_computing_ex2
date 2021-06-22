5acda1d625de58d103d1074cbab018017d577d23
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-22 16:42:12 +0300

    Update ec2_init.sh

--- cachingInTheCloud/src/ec2_init.sh
+++ cachingInTheCloud/src/ec2_init.sh
@@ -2,7 +2,7 @@
 sudo apt update
 
 # installing pip for python 3
-sudo apt --yes --force-yes install python3-pip
+sudo apt --yes install python3-pip
 
 # installing aws cli
 sudo pip3 install --upgrade awscli
