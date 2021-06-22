55516fc5b9dc8238c3657b8df0faafb48411f219
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-22 17:54:04 +0300

    Update elb.py

--- cachingInTheCloud/src/elb.py
+++ cachingInTheCloud/src/elb.py
@@ -184,7 +184,7 @@ def get_instance_ip(instance_id):
         "Name": "instance-id",
         "Values": [instance_id],
     }]
-    return ec2.describe_instances(Filters=filters)['Reservations'][0]['Instances']['PublicDnsName']
+    return ec2.describe_instances(Filters=filters)['Reservations'][0]['Instances']
 def create_ec2_instances(num_instances):
     instances = ec2.run_instances(
           ImageId = 'ami-09e67e426f25ce0d7',
