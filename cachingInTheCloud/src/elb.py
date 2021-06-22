9deb64e7f7f6e4df158b2e906caac9e03e1727fe
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-22 11:59:24 +0300

    Update elb.py

--- cachingInTheCloud/src/elb.py
+++ cachingInTheCloud/src/elb.py
@@ -12,13 +12,13 @@ ec2_user_data = """#cloud-config
 
 runcmd:
 - cd home/ubuntu
-- export AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID") 
-- export AWS_SECRET_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_ACCESS_KEY_ID")
-- export AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION")
 - git clone https://github.com/orzach-idc/cloud_computing_ex2.git
 - cd cloud_computing_ex2/cachingInTheCloud/src
 - chmod 777 *.sh
 - ./ec2_init.sh 
+- export AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID") 
+- export AWS_SECRET_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_ACCESS_KEY_ID")
+- export AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION")
 - sudo python3 elb.py
 - sudo python3 ec2_server.py
 """
