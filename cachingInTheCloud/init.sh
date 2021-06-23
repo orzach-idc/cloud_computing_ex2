e9edf65c252beafb5978d64b1aa6b2d2feb679de
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-23 20:12:46 +0300

    Update init.sh

--- cachingInTheCloud/init.sh
+++ cachingInTheCloud/init.sh
@@ -12,3 +12,8 @@ sudo pip3 install aws-sam-cli
 
 # Configure AWS setup
 sudo aws configure
+
+# setting credentials environment variables
+export AWS_ACCESS_KEY_ID=$(sudo aws configure get aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(sudo aws configure get aws_secret_access_key)
+export AWS_DEFAULT_REGION=$(sudo aws configure get aws_default_region)
