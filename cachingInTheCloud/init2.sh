62d4a24c893083269ce015c1c1efc65e35ab7128
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-25 10:32:20 +0300

    Update init2.sh

--- cachingInTheCloud/init2.sh
+++ cachingInTheCloud/init2.sh
@@ -3,7 +3,7 @@ cd src
 # export environment credentials variables
 export AWS_ACCESS_KEY_ID=$(sudo aws configure get aws_access_key_id)
 export AWS_SECRET_ACCESS_KEY=$(sudo aws configure get aws_secret_access_key)
-export AWS_DEFAULT_REGION=$(sudo sudo aws configure get aws_default_region)
+export AWS_DEFAULT_REGION=$(sudo aws configure get aws_default_region)
 
 # run elb file
 sudo python3 elb.py $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_DEFAULT_REGION
