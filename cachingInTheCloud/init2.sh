696a86c1fe432f4178dfbca926882702cf91e62e
orzach-idc <83864848+orzach-idc@users.noreply.github.com>
2021-06-26 22:13:02 +0300

    Delete init2.sh

--- cachingInTheCloud/init2.sh
+++
@@ -1,7 +0,0 @@
-# export environment credentials variables
-export AWS_ACCESS_KEY_ID=$(sudo aws configure get aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(sudo aws configure get aws_secret_access_key)
-export AWS_DEFAULT_REGION=$(sudo aws configure get region)
-
-# run elb file
-sudo python3 elb.py
